edc5223600f66735bd63c600f8e70f02b1441026
Jackson Geer <jacksongeer@gmail.com>
2023-09-06 16:34:58 -0400

    added fonts and style changes

--- first_flutter_program/lib/main.dart
+++ first_flutter_program/lib/main.dart
@@ -3,16 +3,44 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Hello Nova, This is My First App'),
+          title: const Text(
+            'Hello Nova, This is My First App',
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Color(0xFFF5C937),
+              fontFamily: 'Montserrat',
+            ),
+          ),
           centerTitle: true,
           backgroundColor: const Color(0xFF04578F),
         ),
         body: const Center(
-          child: Text('Hello Nova'),
+          child: Text(
+            'Hello Nova',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.w800,
+              fontStyle: FontStyle.normal,
+              letterSpacing: 2.0,
+              color: Color.fromARGB(255, 0, 0, 0),
+              fontFamily: 'Montserrat',
+            ),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Text('Click'),
-        ),
+            onPressed: () {},
+            backgroundColor: const Color(0xFF04578F),
+            child: const Text(
+              'Click',
+              style: TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                color: Color(0xFFF5C937),
+                fontFamily: 'Montserrat',
+              ),
+            )),
       ),
     ));
