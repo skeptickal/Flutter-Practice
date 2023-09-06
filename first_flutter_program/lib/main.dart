ce4c6920389370c9f2aa01a383442f1213438dad
Jackson Geer <jacksongeer@gmail.com>
2023-09-06 17:15:36 -0400

    added logo to asset folder, put it on the button, and added image asset to pubspec.yaml folder

--- first_flutter_program/lib/main.dart
+++ first_flutter_program/lib/main.dart
@@ -1,46 +1,46 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Hello Nova, This is My First App',
-            style: TextStyle(
-              fontSize: 16.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Color(0xFFF5C937),
-              fontFamily: 'Montserrat',
-            ),
-          ),
-          centerTitle: true,
-          backgroundColor: const Color(0xFF04578F),
+void main() => runApp(
+      MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.blue),
+        darkTheme: ThemeData.dark().copyWith(
+          primaryColor: Colors.black,
+          scaffoldBackgroundColor: Colors.grey[900],
         ),
-        body: const Center(
-          child: Text(
-            'Hello Nova',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.w800,
-              fontStyle: FontStyle.normal,
-              letterSpacing: 2.0,
-              color: Color.fromARGB(255, 0, 0, 0),
-              fontFamily: 'Montserrat',
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            backgroundColor: const Color(0xFF04578F),
-            child: const Text(
-              'Click',
+        themeMode: ThemeMode.dark,
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'Northern VA Church of Christ',
               style: TextStyle(
-                fontSize: 12.0,
-                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+                fontWeight: FontWeight.normal,
                 letterSpacing: 2.0,
-                color: Color(0xFFF5C937),
+                color: Color.fromARGB(255, 255, 255, 255),
                 fontFamily: 'Montserrat',
               ),
-            )),
+            ),
+            centerTitle: true,
+            backgroundColor: const Color(0xFF04578F),
+          ),
+          body: const Center(
+            child: Text(
+              'Hello Member',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.normal,
+                fontStyle: FontStyle.normal,
+                letterSpacing: 2.0,
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontFamily: 'Montserrat',
+              ),
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: Color.fromARGB(255, 255, 255, 255),
+            child: Image.asset('assets/novalogo.png'),
+          ),
+        ),
       ),
-    ));
+    );
