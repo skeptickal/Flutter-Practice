7a868e834db9621f43599bf101e1e32d99cc4fce
Jackson Geer <jacksongeer@gmail.com>
2023-09-06 22:39:29 -0400

    Added Buttons, icons, and other customizations

--- tutorial_flutter_program/lib/main.dart
+++ tutorial_flutter_program/lib/main.dart
@@ -1,59 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text(
-              'My First App',
-              style: TextStyle(
-                fontSize: 16.0,
-                fontWeight: FontWeight.normal,
-                letterSpacing: 2.0,
-                color: Color.fromARGB(255, 255, 255, 255),
-                fontFamily: 'Montserrat',
-              ),
-            ),
-            centerTitle: true,
-            backgroundColor: const Color(0xFF04578F),
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  final logger = Logger();
+  Home({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'My First App',
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.normal,
+            letterSpacing: 2.0,
+            color: Color.fromARGB(255, 255, 255, 255),
+            fontFamily: 'Montserrat',
           ),
-          body: const Center(
-            child: Text(
-              'Hello Jackson',
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.normal,
-                fontStyle: FontStyle.normal,
-                letterSpacing: 2.0,
-                color: Color.fromARGB(255, 0, 0, 0),
-                fontFamily: 'Montserrat',
-              ),
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            backgroundColor: const Color(0xFF04578F),
-            child: const Text(
-              'Hi',
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.normal,
-                fontStyle: FontStyle.normal,
-                letterSpacing: 2.0,
-                color: Color.fromARGB(255, 255, 255, 255),
-                fontFamily: 'Montserrat',
-              ),
-            ),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color(0xFF04578F),
+      ),
+      body: Center(
+        child: IconButton(
+          onPressed: () {
+            logger.i('you clicked me again!');
+          },
+          icon: const Icon(Icons.person_outline_sharp),
+          color: const Color(0xFFF5C937),
+          iconSize: 50.0,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: const Color(0xFF04578F),
+        child: const Text(
+          'Hi',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.normal,
+            fontStyle: FontStyle.normal,
+            letterSpacing: 2.0,
+            color: Color.fromARGB(255, 255, 255, 255),
+            fontFamily: 'Montserrat',
           ),
         ),
       ),
     );
-
-class Test extends StatelessWidget {
-  const Test({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
